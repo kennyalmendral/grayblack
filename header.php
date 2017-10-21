9164199b622ebc1e987c951f13550e46ee9e436c
Kenny Almendral <kennyalmendral@gmail.com>
2017-10-21 14:57:37 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
